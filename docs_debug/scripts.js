68f7012f77d4f402395778d57b39023c41cc70d1
Luke Latham <github@lukelatham.com>
2017-08-13 03:02:35 -0500

    Fix script

--- docs_debug/scripts.js
+++ docs_debug/scripts.js
@@ -20,41 +20,43 @@
     document.querySelector('head').appendChild(msViewportStyle)
   }
 
-  var c = new Array([62,35,255],[60,255,60],[255,35,98],[45,175,230],[255,0,255],[255,128,0]);
-  var s = 0;
-  var ci = [0,1,2,3];
-  var s = 0.002;
-
+  var colors = new Array([62,35,255],[60,255,60],[255,35,98],[45,175,230],[255,0,255],[255,128,0]);
+  var step = 0;
+  var colorIndices = [0,1,2,3];
+  var gradientSpeed = 0.002;
+  
   function updateGradient()
   {
-    var c0_0 = c[ci[0]];
-    var c0_1 = c[ci[1]];
-    var c1_0 = c[ci[2]];
-    var c1_1 = c[ci[3]];
-    var is = 1 - s;
-    var r1 = Math.round(is * c0_0[0] + s * c0_1[0]);
-    var g1 = Math.round(is * c0_0[1] + s * c0_1[1]);
-    var b1 = Math.round(is * c0_0[2] + s * c0_1[2]);
-    var c1 = "rgb("+r1+","+g1+","+b1+")";
-    var r2 = Math.round(is * c1_0[0] + s * c1_1[0]);
-    var g2 = Math.round(is * c1_0[1] + s * c1_1[1]);
-    var b2 = Math.round(is * c1_0[2] + s * c1_1[2]);
-    var c2 = "rgb("+r2+","+g2+","+b2+")";
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb("+r1+","+g1+","+b1+")";
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb("+r2+","+g2+","+b2+")";
 
-    document.getElementById("gradient").style.background = "-webkit-gradient(linear, left top, right top, from("+c1+"), to("+c2+"))";
-    document.getElementById("gradient").style.background = "-moz-linear-gradient(left, "+c1+" 0%, "+c2+" 100%)";
-
-    s += s;
-
-    if ( s >= 1 ) {
-        s %= 1;
-        ci[0] = ci[1];
-        ci[2] = ci[3];
-        ci[1] = ( ci[1] + Math.floor( 1 + Math.random() * (c.length - 1))) % c.length;
-        ci[3] = ( ci[3] + Math.floor( 1 + Math.random() * (c.length - 1))) % c.length;
+    document.getElementById("gradient").style.background = "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))";
+    document.getElementById("gradient").style.background = "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)";
+  
+    step += gradientSpeed;
+    
+    if ( step >= 1 ) {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+        colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
     }
   }
   
-  setInterval(updateGradient,10);
+  if ( location.pathname == "/" || location.pathname == "/index.html" ) {
+    setInterval(updateGradient,10);
+  }
 
 })();
