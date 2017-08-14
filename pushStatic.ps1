2f1fd84af846b4457a1480e8ec8ee3c3c69417ec
Luke Latham <github@lukelatham.com>
2017-08-13 23:02:54 -0500

    Script update 5

--- pushStatic.ps1
+++ pushStatic.ps1
@@ -9,32 +9,44 @@ Write-Host "- Clone master branch...."
 cd "$($buildFolder)\..\"
 mkdir gh-deploy
 git clone --quiet --branch=master https://$($username):$($personalAccessToken)@github.com/guardrex/guardrex.com.git .\gh-deploy\
-cd gh-deploy
+cd gh-deploy\docs
 
+Write-Host
+Write-Host "Contents of gh-deploy\docs:"
 dir
 
+Write-Host
+Write-Host "Status:"
 git status
 
-#Write-Host "- Clean docs folder...."
-#Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
+Write-Host "- Clean docs folder...."
+Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
 
-Write-Host $pwd.Path
+Write-Host
+Write-Host "Contents of gh-deploy\docs after clean:"
+dir
 
 Write-Host "- Copy contents of guardrex-com into docs folder...."
-copy-item -path ..\guardrex-com\* -Destination $pwd.Path -Recurse
+copy-item -path ..\guardrex-com\docs\* -Destination $pwd.Path\docs -Recurse
 
+Write-Host
+Write-Host "Contents of gh-deploy\docs after copy:"
+dir
+
+Write-Host
+Write-Host "Status:"
 git status
+
 $thereAreChanges = git status | select-string -pattern "Changes not staged for commit:","Untracked files:" -simplematch
 if ($thereAreChanges -ne $null) { 
-    Write-host "- Committing changes to documentation..."
+    Write-host "- Committing changes..."
     #git add --all
     #git status
     #git commit -m "skip ci - static site regeneration"
     #git status
-    Write-Host "- Push it...."
     #git push --quiet
-    Write-Host "- Pushed it good!"
+    Write-Host "- Done!"
 } 
 else { 
-    write-host "- No changes to documentation to commit"
+    write-host "- No changes to commit"
 }
