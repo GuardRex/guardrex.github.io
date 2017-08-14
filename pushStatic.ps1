51061c66eca3e15a66b980e8c30dad352586e8d4
Luke Latham <github@lukelatham.com>
2017-08-13 23:21:31 -0500

    Script update 7

--- pushStatic.ps1
+++ pushStatic.ps1
@@ -11,37 +11,26 @@ mkdir gh-deploy
 git clone --quiet --branch=master https://$($username):$($personalAccessToken)@github.com/guardrex/guardrex.com.git .\gh-deploy\
 cd gh-deploy\docs
 
-Write-Host
-Write-Host "Status:"
-git status
-
-Write-Host
-Write-Host "- Clean docs folder...."
+Write-Host "- Clean docs folder..."
 Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
-
-Write-Host
-Write-Host "Contents of gh-deploy\docs after clean:"
-dir
 
 Write-Host "- Copy contents of guardrex-com into docs folder...."
 copy-item -path c:\projects\guardrex-com\docs\* -Destination c:\projects\gh-deploy\docs -Recurse
 
-Write-Host
-Write-Host "Contents of gh-deploy\docs after copy:"
-dir
-
-Write-Host
-Write-Host "Status:"
+Write-Host "- Status:"
 git status
 
 $thereAreChanges = git status | select-string -pattern "Changes not staged for commit:","Untracked files:" -simplematch
 if ($thereAreChanges -ne $null) { 
     Write-host "- Committing changes..."
-    #git add --all
-    #git status
-    #git commit -m "skip ci - static site regeneration"
-    #git status
-    #git push --quiet
+    git add --all
+    Write-Host "- Status:"
+    git status
+    git commit -m "skip ci - static site regeneration"
+    Write-Host "- Status:"
+    git status
+    Write-host "- Pushing commit..."
+    git push --quiet
     Write-Host "- Done!"
 } 
 else { 
