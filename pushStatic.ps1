95f2c101fcd70914b5dbb43c4caf38d9e0082b37
Luke Latham <github@lukelatham.com>
2017-08-20 18:03:54 -0500

    AppVeyor config and script updates

--- pushStatic.ps1
+++ pushStatic.ps1
@@ -11,46 +11,61 @@ mkdir gh-deploy
 git clone --quiet --branch=master https://$($username):$($personalAccessToken)@github.com/guardrex/guardrex.com.git .\gh-deploy\
 cd gh-deploy\docs
 
+Write-Host
 Write-Host "1 contents gh-deploy\docs..."
 dir
 
+Write-Host
 Write-Host "2 contents gh-deploy\docs\post..."
 dir post
 
+Write-Host
 Write-Host "- Clean docs folder..."
 Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
 
+Write-Host
 Write-Host "3 directory of c:\projects\guardrex-com\docs_staging..."
 dir c:\projects\guardrex-com\docs_staging
 
+Write-Host
 Write-Host "4 directory of c:\projects\guardrex-com\docs_staging\post..."
 dir c:\projects\guardrex-com\docs_staging\post
 
+Write-Host
 Write-Host "- Copy contents of guardrex-com into docs folder...."
 copy-item -path c:\projects\guardrex-com\docs_staging\* -Destination c:\projects\gh-deploy\docs -Recurse -Force
 
+Write-Host
 Write-Host "5 directory of gh-deploy\docs after copy..."
 dir
 
+Write-Host
 Write-Host "6 directory of gh-deploy\docs\post after copy..."
 dir post
 
+Write-Host
 Write-Host "- Status:"
 git status
 
 $thereAreChanges = git status | select-string -pattern "Changes not staged for commit:","Untracked files:" -simplematch
-if ($thereAreChanges -ne $null) { 
-    Write-host "- Committing changes..."
+if ($thereAreChanges -ne $null) {
+    Write-Host
+    Write-Host "- Committing changes..."
     git add --all
+    Write-Host
     Write-Host "- Status:"
     git status
     git commit -m "[ci skip] GitHub Pages regeneration"
+    Write-Host
     Write-Host "- Status:"
     git status
-    Write-host "- Pushing commit..."
+    Write-Host
+    Write-Host "- Pushing commit..."
     git push --quiet
+    Write-Host
     Write-Host "- Done!"
 } 
-else { 
-    write-host "- No changes to commit"
+else {
+    Write-Host
+    Write-Host "- No changes to commit"
 }
