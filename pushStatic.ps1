22ca58f4d0f459bcf726c74913b9b562d955de88
Luke Latham <github@lukelatham.com>
2017-08-20 17:40:44 -0500

    More data

--- pushStatic.ps1
+++ pushStatic.ps1
@@ -11,19 +11,28 @@ mkdir gh-deploy
 git clone --quiet --branch=master https://$($username):$($personalAccessToken)@github.com/guardrex/guardrex.com.git .\gh-deploy\
 cd gh-deploy\docs
 
-Write-Host "contents gh-deploy\docs\post..."
+Write-Host "1 contents gh-deploy\docs..."
+dir
+
+Write-Host "2 contents gh-deploy\docs\post..."
 dir post
 
 Write-Host "- Clean docs folder..."
 Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
 
-Write-Host "directory of c:\projects\guardrex-com\docs_staging\post..."
+Write-Host "3 directory of c:\projects\guardrex-com\docs_staging..."
+dir c:\projects\guardrex-com\docs_staging
+
+Write-Host "4 directory of c:\projects\guardrex-com\docs_staging\post..."
 dir c:\projects\guardrex-com\docs_staging\post
 
 Write-Host "- Copy contents of guardrex-com into docs folder...."
-copy-item -path c:\projects\guardrex-com\docs_staging\* -Destination c:\projects\gh-deploy\docs -Recurse
+copy-item -path c:\projects\guardrex-com\docs_staging\* -Destination c:\projects\gh-deploy\docs -Recurse -Force
 
-Write-Host "directory of gh-deploy\docs\post after copy contents c:\projects\guardrex-com\docs_staging TO gh-deploy\docs..."
+Write-Host "5 directory of gh-deploy\docs after copy..."
+dir
+
+Write-Host "6 directory of gh-deploy\docs\post after copy..."
 dir post
 
 Write-Host "- Status:"
